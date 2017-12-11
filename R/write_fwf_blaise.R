1a82261f8876e68f1861c64272bb0701023973aa
Ophof <SECSOHF@cbsp.nl>
2017-12-11 10:47:47 +0100

    implemented write interface

---
+++ R/write_fwf_blaise.R
@@ -0,0 +1,7 @@
+write_fwf_blaise = function(df,
+                            output_data,
+                            output_model = NULL,
+                            locale = readr::locale(decimal_mark = ','),
+                            force_string = FALSE){
+
+}
